13078eb3d417fd6d4447efc6c5ce3af50aa33fd2
Gajus Kuizinas <gk@anuary.com>
2015-08-30 16:19:39 +0100

    Less clutter when setting the configuration.

--- dist/utils.js
+++ dist/utils.js
@@ -6,21 +6,11 @@ Object.defineProperty(exports, '__esModule', {
 
 function _interopRequireDefault(obj) { return obj && obj.__esModule ? obj : { 'default': obj }; }
 
-var _lodashObjectKeys = require('lodash/object/keys');
-
-var _lodashObjectKeys2 = _interopRequireDefault(_lodashObjectKeys);
-
-var _lodashArrayDifference = require('lodash/array/difference');
-
-var _lodashArrayDifference2 = _interopRequireDefault(_lodashArrayDifference);
-
 var _lodashCollectionForEach = require('lodash/collection/forEach');
 
 var _lodashCollectionForEach2 = _interopRequireDefault(_lodashCollectionForEach);
 
 exports['default'] = {
-    keys: _lodashObjectKeys2['default'],
-    difference: _lodashArrayDifference2['default'],
     forEach: _lodashCollectionForEach2['default']
 };
 module.exports = exports['default'];
