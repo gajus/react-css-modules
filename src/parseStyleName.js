6b9c8a4e0797c585a82b548e463e00c58f16c6d0
Gajus Kuizinas <gk@anuary.com>
2016-02-02 17:52:06 +0000

    Use object to cache styleName translations.

--- src/parseStyleName.js
+++ src/parseStyleName.js
@@ -1,10 +1,20 @@
 import _ from 'lodash';
 
+let styleNameIndex;
+
+styleNameIndex = {};
+
 export default (styleNamePropertyValue: string, allowMultiple: boolean): Array<string> => {
     let styleNames;
 
-    styleNames = styleNamePropertyValue.split(' ');
-    styleNames = _.filter(styleNames);
+    if (styleNameIndex[styleNamePropertyValue]) {
+        styleNames = styleNameIndex[styleNamePropertyValue];
+    } else {
+        styleNames = _.trim(styleNamePropertyValue).split(' ');
+        styleNames = _.filter(styleNames);
+
+        styleNameIndex[styleNamePropertyValue] = styleNames;
+    }
 
     if (allowMultiple === false && styleNames.length > 1) {
         throw new Error('ReactElement styleName property defines multiple module names ("' + styleNamePropertyValue + '").');
